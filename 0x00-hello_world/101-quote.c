49ed0885665a54376ec4043c6c52e19a3d3f7d32
Nomfundomlangeni <mlangenin67@gmail.com>
2023-02-20 15:03:31 -0800

    4-puts.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,15 +1,17 @@
-#include <unistd.h>
+#include <stdio.h>
 
 /**
  * main - prints"and that piece of art is useful" - Dora Korpar, 2015-10-19",
- * followed by a new line, to the standard error.
+ * followed by a new line, to the standard error"
  * Return: Always 1.
+ *
  */
 
 
 int main(void)
 
 {
-fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+char * and that piece of art is useful\ - Dora Korpar, 2015-10-19
 return (1);
+
 }
