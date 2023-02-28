cdc7c2dc78b33d8a9a3a0cec45212ffbcec6da66
Nomfundomlangeni <mlangenin67@gmail.com>
2023-02-27 16:59:37 -0800

    3-puts.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,12 +1,13 @@
 #include "main.h"
 
 /**
- * _put - prints a string
+ * _puts - prints a string
  * @str: the string to print
  * Return: void
  */
 
 void _puts(char *str)
+
 {
 	for (; *str != '\0'; str++)
 	{
