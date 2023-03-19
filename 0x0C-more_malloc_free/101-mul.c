848926b4d20d4a0e62894e1f3354ac1e61eb58be
Nomfundomlangeni <mlangenin67@gmail.com>
2023-03-18 20:32:09 -0700

    101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -6,6 +6,7 @@
 /**
  * _is_zero - determines if any number is zero
  * @argv: argument vector.
+ *
  * Return: no return.
  */
 void _is_zero(char *argv[])
@@ -25,16 +26,19 @@ void _is_zero(char *argv[])
 			isn2 = 0;
 			break;
 		}
+
 	if (isn1 == 1 || isn2 == 1)
 	{
 		printf("0\n");
 		exit(0);
 	}
 }
+
 /**
  * _initialize_array - set memery to zero in a new array
  * @ar: char array.
  * @lar: length of the char array.
+ *
  * Return: pointer of a char array.
  */
 char *_initialize_array(char *ar, int lar)
@@ -46,11 +50,13 @@ char *_initialize_array(char *ar, int lar)
 	ar[lar] = '\0';
 	return (ar);
 }
+
 /**
  * _checknum - determines length of the number
  * and checks if number is in base 10.
  * @argv: arguments vector.
  * @n: row of the array.
+ *
  * Return: length of the number.
  */
 int _checknum(char *argv[], int n)
@@ -63,13 +69,16 @@ int _checknum(char *argv[], int n)
 			printf("Error\n");
 			exit(98);
 		}
+
 	return (ln);
 }
+
 /**
  * main - Entry point.
  * program that multiplies two positive numbers.
  * @argc: number of arguments.
  * @argv: arguments vector.
+ *
  * Return: 0 - success.
  */
 int main(int argc, char *argv[])
@@ -102,15 +111,15 @@ int main(int argc, char *argv[])
 		{
 			if (nout[0] != '0')
 				break;
-	lnout--;
-	free(nout), nout = malloc(lnout + 1), nout = _initialize_array(nout, lnout);
-	k = lnout - 1, i = ln1 - 1, j = ln2 - 1, ca = addl = 0;
-	}
-	if (j >= 0)
-	{
-	add = ((argv[1][i] - '0') * (argv[2][j] - '0')) + (nout[k] - '0') + addl;
-	addl = add / 10, nout[k] = (add % 10) + '0';
-	}
+			lnout--;
+			free(nout), nout = malloc(lnout + 1), nout = _initialize_array(nout, lnout);
+			k = lnout - 1, i = ln1 - 1, j = ln2 - 1, ca = addl = 0;
+		}
+		if (j >= 0)
+		{
+			add = ((argv[1][i] - '0') * (argv[2][j] - '0')) + (nout[k] - '0') + addl;
+			addl = add / 10, nout[k] = (add % 10) + '0';
+		}
 	}
 	printf("%s\n", nout);
 	return (0);
