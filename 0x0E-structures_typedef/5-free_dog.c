58f37d0e6de2e56a029e1fa08c5497620d05970b
Nomfundomlangeni <mlangenin67@gmail.com>
2023-03-21 17:51:34 -0700

    5-free_dog.c

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -7,11 +7,12 @@
 * @d: struct dog to free
 */
 void free_dog(dog_t *d)
-{
-	if (d)
-{
-		free(d->name);
-		free(d->owner);
-		free(d);
-}
+	{
+		if (d)
+		{
+			free(d->name);
+			free(d->owner);
+			free(d);
+		}
+	}
 
